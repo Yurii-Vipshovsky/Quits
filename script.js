10f20e1d4880178254d24cb9d3b2b665a877a7e5
Yurii-Vipshovsky <70889183+Yurii-Vipshovsky@users.noreply.github.com>
2025-11-19 14:13:45 +0200

    Update script.js
    Signed-off-by: Yurii-Vipshovsky <70889183+Yurii-Vipshovsky@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const BUILTIN_QUIZZES = [
   // приклад:
-  { id: "vocab1", name: "Старі тести 230шт", path: "quiz.json" },
-  { id: "emergency", name: "Нові тести", path: "quiz1.json" },
-  { id: "emergencyPreparation", name: "Долікарська підготовка", path: "quiz2.json" },
-   { id: "emergencyPreparation", name: "Долікарська підготовка нова", path: "quiz3.json" },
+  { id: "emergencyPreparation", name: "Долікарська підготовка нова", path: "quiz3.json" },
 ];
 
 function $(sel) {
